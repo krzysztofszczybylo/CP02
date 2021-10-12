22c44d9d62555060bff436e51d7a87858457f674
krzysztofszczybylo <s218037@student.uek.krakow.pl>
2021-10-13 00:57:37 +0200

    Poprawione zadanie 17

--- zadanie17/Counter.java
+++ zadanie17/Counter.java
@@ -5,12 +5,41 @@ public class Counter
     int lastValue;
     
     void Reset(){
-        System.out.println(initialValue);
+        System.out.println(initialValue = 0);
     };
     void Count(){
-        System.out.println(
-            if (lastValue > 0);
-                if i < 10
-        );
-    }
-}
+        
+            if (lastValue > 0){
+                for (initialValue = 0; (initialValue-lastValue) < 11; initialValue = initialValue+10){
+                    System.out.println(initialValue);
+                    if ((lastValue - initialValue) < 10){
+                        for (initialValue = initialValue; initialValue <= lastValue; initialValue++){
+                            
+                                System.out.println(initialValue);
+                                
+                            }
+                        }
+                        
+                    }
+                    
+
+                } else {
+                    for (initialValue = 0; (initialValue-lastValue) >= -10; initialValue = initialValue-10){
+                    System.out.println(initialValue);
+                    if ((lastValue - initialValue) < 10){
+                        for (initialValue = initialValue; initialValue >= lastValue; initialValue--){
+                            
+                                System.out.println(initialValue);
+                                
+                    
+                        }
+                
+                    
+                    }
+                
+                
+        
+                    }
+                }
+            }
+        }
